19c7df845787d0749ff7e9f9a7df0362e72198ee
Alastair <alastair.r.mcneill@gmail.com>
2022-01-06 21:41:38 +0000

    added message data

--- lib/models/message_model.dart
+++ lib/models/message_model.dart
@@ -4,12 +4,21 @@ class Message {
   final String text;
   final String time;
   final User sender;
+  final bool unread;
 
   Message({
     required this.text,
     required this.time,
     required this.sender,
+    required this.unread,
   });
 }
 
-List<Message> conversation = [];
+List<Message> conversation = [
+  Message(
+    sender: cameronAgnew,
+    time: '5:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    unread: true,
+  ),
+];
